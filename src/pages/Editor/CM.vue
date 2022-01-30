398c6d99052c95d58baf169fb6493e063b42316f
idanran <idanran@outlook.com>
2022-01-30 21:16:11 +0800

    似乎好起来了

--- src/pages/Editor/CM.vue
+++ src/pages/Editor/CM.vue
@@ -3,90 +3,132 @@
         <div class="edit-content">
             <n-space vertical>
                 <div class="edit-content-toolbar">
-                    <n-tooltip>
-                        <template #trigger>
-                            <n-button quaternary @click="undo">
-                                <template #icon>
-                                    <n-icon>
-                                        <undo-icon />
-                                    </n-icon>
-                                </template>
-                            </n-button>
-                        </template>
-                        撤销
-                    </n-tooltip>
-                    <n-tooltip>
-                        <template #trigger>
-                            <n-button quaternary @click="redo">
-                                <template #icon>
-                                    <n-icon>
-                                        <redo-icon />
-                                    </n-icon>
-                                </template>
-                            </n-button>
-                        </template>
-                        重做
-                    </n-tooltip>
-                    <n-tooltip>
-                        <template #trigger>
-                            <n-button quaternary @click="bold">
-                                <template #icon>
-                                    <n-icon>
-                                        <bold-icon />
-                                    </n-icon>
-                                </template>
-                            </n-button>
-                        </template>
-                        粗体
-                    </n-tooltip>
-                    <n-tooltip>
-                        <template #trigger>
-                            <n-button quaternary @click="italic">
-                                <template #icon>
-                                    <n-icon>
-                                        <italic-icon />
-                                    </n-icon>
-                                </template>
-                            </n-button>
-                        </template>
-                        斜体
-                    </n-tooltip>
-                    <n-tooltip>
-                        <template #trigger>
-                            <n-button quaternary @click="link">
-                                <template #icon>
-                                    <n-icon>
-                                        <link-icon />
-                                    </n-icon>
-                                </template>
-                            </n-button>
-                        </template>
-                        链接
-                    </n-tooltip>
-                    <n-tooltip>
-                        <template #trigger>
-                            <n-button quaternary @click="quote">
-                                <template #icon>
-                                    <n-icon>
-                                        <quotes-icon />
-                                    </n-icon>
-                                </template>
-                            </n-button>
-                        </template>
-                        引用
-                    </n-tooltip>
-                    <n-tooltip>
-                        <template #trigger>
-                            <n-button quaternary @click="chem">
-                                <template #icon>
-                                    <n-icon>
-                                        <flask-icon />
-                                    </n-icon>
-                                </template>
-                            </n-button>
-                        </template>
-                        化学式
-                    </n-tooltip>
+                    <div class="edit-content-toolbar-group">
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="undo" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <undo-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            撤销
+                        </n-tooltip>
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="redo" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <redo-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            重做
+                        </n-tooltip>
+                    </div>
+                    <div class="edit-content-toolbar-group">
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="bold" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <bold-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            粗体
+                        </n-tooltip>
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="italic" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <italic-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            斜体
+                        </n-tooltip>
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="strikethrough" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <strikethrough-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            删除线
+                        </n-tooltip>
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="code" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <code-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            程序代码
+                        </n-tooltip>
+                    </div>
+                    <div class="edit-content-toolbar-group">
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="link" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <link-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            链接
+                        </n-tooltip>
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="quote" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <quotes-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            引用文献
+                        </n-tooltip>
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="chem" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <flask-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            化学式
+                        </n-tooltip>
+                        <n-tooltip>
+                            <template #trigger>
+                                <n-button quaternary @click="math" size="large">
+                                    <template #icon>
+                                        <n-icon>
+                                            <percentage-icon />
+                                        </n-icon>
+                                    </template>
+                                </n-button>
+                            </template>
+                            数学公式
+                        </n-tooltip>
+                    </div>
                 </div>
             </n-space>
             <div class="edit-content-editor" ref="editor"></div>
@@ -96,16 +138,18 @@
 
 <script setup>
 import { NSpace, NButton, NTooltip, NIcon } from 'naive-ui'
-import { Undo as UndoIcon, Redo as RedoIcon, Bold as BoldIcon, Italic as ItalicIcon, Link as LinkIcon, QuoteLeft as QuotesIcon, Flask as FlaskIcon } from "@vicons/fa"
+import { Undo as UndoIcon, Redo as RedoIcon, Bold as BoldIcon, Italic as ItalicIcon, Link as LinkIcon, QuoteLeft as QuotesIcon, Flask as FlaskIcon, Strikethrough as StrikethroughIcon, Percentage as PercentageIcon, Code as CodeIcon } from "@vicons/fa"
 
 import { defineComponent } from 'vue';
 import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup"
 import { undo as cmUndo, redo as cmRedo } from "@codemirror/history"
 import { EditorSelection } from "@codemirror/state"
+import { defaultHighlightStyle } from "@codemirror/highlight";
+import { html } from "@codemirror/lang-html"
 
 import { ref, onMounted } from "vue"
 
-const code = ref("");
+//const code = ref("");
 const editor = ref(null);
 let editorView = null;
 
@@ -145,12 +189,30 @@ const chem = () => {
         range: EditorSelection.range(range.from + 6, range.to + 6)
     })))
 };
+const strikethrough = () => {
+    editorView.dispatch(editorView.state.changeByRange(range => ({
+        changes: [{ from: range.from, insert: "<s>" }, { from: range.to, insert: "</s>" }],
+        range: EditorSelection.range(range.from + 3, range.to + 3)
+    })))
+};
+const math = () => {
+    editorView.dispatch(editorView.state.changeByRange(range => ({
+        changes: [{ from: range.from, insert: "<math>" }, { from: range.to, insert: "</math>" }],
+        range: EditorSelection.range(range.from + 6, range.to + 6)
+    })))
+};
+const code = () => {
+    editorView.dispatch(editorView.state.changeByRange(range => ({
+        changes: [{ from: range.from, insert: "<code>" }, { from: range.to, insert: "</code>" }],
+        range: EditorSelection.range(range.from + 6, range.to + 6)
+    })))
+};
 
 onMounted(() => {
     editorView = new EditorView({
         state: EditorState.create({
-            doc: code.value,
-            extensions: [basicSetup],
+            //            doc: code.value,
+            extensions: [basicSetup, defaultHighlightStyle, html()],
         }),
         parent: editor.value,
     });
@@ -170,5 +232,12 @@ onMounted(() => {
 }
 .edit-content-toolbar {
     background-color: #fafafa;
+}
+.edit-content-toolbar-group {
+    display: inline-block;
+    border-right: 1px solid #eaecf0;
+}
+.edit-content-toolbar-group:last-child {
+    border-right: 0;
 }
 </style>
